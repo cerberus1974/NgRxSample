0cbb4cffd719ab0b55538d054d42f4178d31ebbd
t_takano <cerberus1974@gmail.com>
2020-08-16 14:11:00 +0900

    reducerのテスト

--- src/app/store/message/reducers/message.reducer.spec.ts
+++ src/app/store/message/reducers/message.reducer.spec.ts
@@ -1,4 +1,6 @@
-import { reducer, initialState } from './message.reducer';
+import { reducer, initialState, State } from './message.reducer';
+import { Actions } from '../actions/message.actions';
+import { Message } from '../message.model';
 
 describe('Message Reducer', () => {
   describe('an unknown action', () => {
@@ -8,6 +10,29 @@ describe('Message Reducer', () => {
       const result = reducer(initialState, action);
 
       expect(result).toBe(initialState);
+    });
+  });
+
+  describe('sendMessage', () => {
+    it('ローディング中になる', () => {
+      const state = { ...initialState, loading: false, success: true };
+      const action = { type: Actions.SendMessage };
+      const exepcted = { ...initialState, loading: true, success: false };
+
+      expect(reducer(state, action)).toEqual(exepcted);
+    });
+  });
+
+  describe('getAllMessageSuccess', () => {
+    it('messagesがStoreにセットされる', () => {
+      const messages = [ { id: 1 } as Message, { id: 2 } as Message ];
+      const state = { ...initialState, loading: true };
+      const action = { type: Actions.GetAllMessageSuccess, messages };
+
+      const actual = reducer(state, action);
+
+      expect(actual.ids).toEqual([1, 2]);
+      expect(actual.loading).toBeFalse();
     });
   });
 });
